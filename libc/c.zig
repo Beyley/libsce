817b6ce39410184fc71b9204c73640d9edbf7a67
Beyley Thomas <ep1cm1n10n123@gmail.com>
2024-10-22 10:26:43 -0700

    Make our libc fill-ins use Zig builtins

--- libc/c.zig
+++ libc/c.zig
@@ -1,15 +1,9 @@
-//! Simple providers for libc functions
-
-// NOTE: we *cannot* use @memset and @memcpy because they try to call into libc, which is an infinite loop!!!
+//! Simple fill-ins for libc functions, since we dont want a hard dependency on libc
 
 pub export fn libc_memcpy(dst: [*]u8, src: [*]u8, n: c_ulong) void {
-    for (dst[0..n], src[0..n]) |*d, s| {
-        d.* = s;
-    }
+    @memcpy(dst[0..n], src[0..n]);
 }
 
 pub export fn libc_memset(dst: [*]u8, val: c_int, n: c_ulong) void {
-    for (dst[0..n]) |*d| {
-        d.* = @intCast(val);
-    }
+    @memset(dst[0..n], @intCast(val));
 }
